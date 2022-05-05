4fc0f2357c515e166746f69a598ef832a8eb3426
Basil Reji <84758142+basil-reji@users.noreply.github.com>
2022-05-05 14:44:58 +0530

    admin side design completed

---
+++ public/javascripts/admin.js
@@ -0,0 +1,6 @@
+$("#harmburger").click(function () {
+    $("#harmburger").toggleClass('active')
+    $("#sidebar").toggleClass('close')
+    $("#header").toggleClass('shrink')
+    $("#main").toggleClass('shrink')
+});
--- routes/admins.js
+++ routes/admins.js
@@ -1,11 +1,44 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var adminHelper = require("../helpers/adminHelper")
-var functionHelper = require("../helpers/functionHelper")
+var adminHelper = require("../helpers/adminHelper");
+var functionHelper = require("../helpers/functionHelper");
 
-/* GET admins listing. */
-router.get('/', function(req, res, next) {
-  res.render('admin/admin', { title: 'Admin' });
+var websiteName = "Website Name";
+
+/* GET dashboard listing. */
+router.get("/", function (req, res, next) {
+    res.render("admin/dashboard", {
+        title: `Admin | ${websiteName}`,
+        isAdmin: true,
+        isDashboardPage: true,
+    });
+});
+
+/* GET dashboard listing. */
+router.get("/dashboard", function (req, res, next) {
+    res.render("admin/dashboard", {
+        title: `Admin | ${websiteName}`,
+        isAdmin: true,
+        isDashboardPage: true,
+    });
+});
+
+/* GET Messages. */
+router.get("/messages", function (req, res, next) {
+    res.render("admin/messages", {
+        title: `Admin | ${websiteName}`,
+        isAdmin: true,
+        isMessagesPage: true,
+    });
+});
+
+/* GET Admins. */
+router.get("/admins", function (req, res, next) {
+    res.render("admin/admins/admins", {
+        title: `Admin | ${websiteName}`,
+        isAdmin: true,
+        isAdminsPage: true,
+    });
 });
 
 module.exports = router;
